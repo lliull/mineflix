b8b4d1d193fce50f86cf78828fb717accff1e7e5
lliull/nemonemo <lliull@naver.com>
2020-07-10 18:01:01 +0900

    0710

---
+++ src/routes/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./About.css"
+
+function About() {
+    return <div className="about_container">
+        <span>Most of the amazing and interesting things that movies give are determined from the first scene. There aren't many audiences who can stand and watch a movie that doesn't have a vision of how to start. -Alfred Hitchcock</span>
+    </div>
+}
+
+export default About;
---
+++ src/routes/Detail.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Detail.css"
+
+class Detail extends React.Component{
+    componentDidMount() {
+        const {location, history} = this.props;
+        if(location.state === undefined){
+            history.push("/");
+        }
+    }
+    render() {
+        const {location} = this.props;
+        if(location.state) {
+            return (
+                <div>
+                    <div className="detail__back">
+                    <img src={location.state.poster} alt={location.state.title} />
+                    </div>
+                    <div className="detail__container">
+                    <img className="detail__poster" src={location.state.poster} alt={location.state.title} />
+                    <h3 className="detail__title">{location.state.title}</h3>
+                    <h5 className="detail__year">{location.state.year}</h5>
+                <ul className="detail__geners">
+                    {location.state.genres.map((genre, index) => <li key={index} className="genres__genre">  {genre} </li> )}
+                </ul>
+                <p className="detail__summary">{location.state.summary}</p>
+                    </div>
+                </div>
+            )
+        }else{
+            return null;
+        }
+    }
+}
+
+export default Detail;
